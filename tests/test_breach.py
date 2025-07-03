852b147bd01b9270454049e67b823e92a044b2b3
GitHub Action <you@example.com>
2025-07-03 20:59:53 +0330

    some changes on tests

--- tests/test_breach.py
+++ tests/test_breach.py
@@ -4,6 +4,10 @@ def test_breached_password():
     count = check_breach("123456")
     assert isinstance(count,(int, type(None)))
 
+def test_breached_empty_password():
+    count = check_breach("")
+    assert isinstance(count,(int, type(None)))
+
 def test_safe_password():
     count = check_breach("M@hdi_Unique#123456")
     assert isinstance(count, (int, type(None)))
