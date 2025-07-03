362a000cd122a8c26ee2f44c21f4b5513ddd91b3
GitHub Action <you@example.com>
2025-07-03 20:47:12 +0330

    c

--- tests/test_strength.py
+++ tests/test_strength.py
@@ -11,13 +11,13 @@ def test_common_password():
     assert score <= 1
 
 def test_medium_password():
-    label, score = score_password("Mahdi123")
-    assert label == "Strong"
-
-def test_medium_password():
     label, score = score_password("mahdi123") 
     assert label == "Medium"
     assert score == 3
+
+def test_strong_password_uppercase():
+    label, score = score_password("Mahdi123")
+    assert label == "Strong"
 
 def test_strong_password():
     label, score = score_password("M@hd1_998877abc")
