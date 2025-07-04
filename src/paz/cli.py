f4277cb56f5bf18cf6eade01acc1b235813de269
GitHub Action <you@example.com>
2025-07-04 09:08:08 +0330

    del space and change v to 1.0.1

--- src/paz/cli.py
+++ src/paz/cli.py
@@ -39,7 +39,7 @@ def analyze_password(password: str):
 
     breach_count = check_breach(password)
     if breach_count is None:
-        print("[bold red]  Breach check failed (network error)[/bold red]")
+        print("[bold red] Breach check failed (network error)[/bold red]")
     elif breach_count == 0:
         print("[bold green] Password not found in known breaches[/bold green]")
     else:
